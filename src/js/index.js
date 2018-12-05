fdb916fdd033c5c228719a5dd89059bffc13340b
asaduzzaman1517 <asaduzzaman1517@gmail.com>
2018-12-05 16:58:22 +0600

    Style index & preloader added

--- src/js/index.js
+++ src/js/index.js
@@ -6,6 +6,10 @@ import '../../node_modules/bootstrap/dist/js/bootstrap';
 //Owl.carousel
 import '../../node_modules/owl.carousel/dist/owl.carousel.min';
 
+//Favicon
+import '../assets/img/favicon.ico';
+import '../assets/img/apple-icon.png'
+
 //Document Ready Function
 $(function(){
     $('.testimonial__slider').owlCarousel({
@@ -15,4 +19,8 @@ $(function(){
         items: 1,
         autoplay: true
     });
+});
+//Preloader
+$(window).on('load', function(){
+    $('.preloader').fadeOut(500);
 });
